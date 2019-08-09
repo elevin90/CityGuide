745dc30f2cb7d5571cce56ed48ad6e2298c6ab28
Evgeny Levin <evgeniy.levin@helmes.com>
2019-08-09 11:07:33 +0300

    Initial Commit

---
+++ CityGuide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityGuide
+//
+//  Created by Evgeniy Levin on 8/9/19.
+//  Copyright © 2019 Evgeniy Levin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
